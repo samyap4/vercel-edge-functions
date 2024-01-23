8d90f085b143ab146a8cf24ae1b30f68c4aa2216
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-01-22 19:22:48 -0600

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { checkAudience } from '@/utils/token_utils';
-import next from 'next';
 
 export const config = {
     matcher: '/api/fga-check',
@@ -8,9 +7,10 @@ export const config = {
 
 export default async function middleware(req: NextRequest) {
     const headers = req.headers;
+    console.log(headers);
     const token = headers.get('Authorization')?.split(' ')[1];
     if (token && checkAudience(token, 'http://localhost:8080')) {
         return NextResponse.next();
     }
-    return NextResponse.json({ message: 'Auth required' }, { status: 401 })
+    return NextResponse.json({ message: headers }, { status: 401 })
 }
