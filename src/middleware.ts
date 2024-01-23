ad48741b3db9e5da68fd8347f12ddaf8b19ba95b
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-01-22 19:32:36 -0600

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { checkAudience } from '@/utils/token_utils';
+import { checkIssuer } from '@/utils/token_utils';
 
 export const config = {
     matcher: '/api/fga-check',
@@ -7,7 +7,7 @@ export const config = {
 
 export default async function middleware(req: NextRequest) {
     const token = req.headers.get('authorization')?.split(' ')[1];
-    if (token && checkAudience(token, 'http://localhost:8080')) {
+    if (token && checkIssuer(token, 'https://auth.samyap.dev/')) {
         return NextResponse.next();
     }
     return NextResponse.json({ message: token }, { status: 401 })
