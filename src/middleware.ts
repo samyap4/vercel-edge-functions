ea9cecf501f11ea03b9a3d818e45e26d42e8a758
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-01-22 19:24:42 -0600

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,11 +6,11 @@ export const config = {
 };
 
 export default async function middleware(req: NextRequest) {
-    const headers = req.headers;
-    console.log(headers);
-    const token = headers.get('Authorization')?.split(' ')[1];
+    const requestHeaders = new Headers(req.headers)
+    console.log(requestHeaders);
+    const token = requestHeaders.get('Authorization')?.split(' ')[1];
     if (token && checkAudience(token, 'http://localhost:8080')) {
         return NextResponse.next();
     }
-    return NextResponse.json({ message: headers }, { status: 401 })
+    return NextResponse.json({ message: requestHeaders }, { status: 401 })
 }
