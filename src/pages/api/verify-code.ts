d2aad9c8ca2415a346c0b5b1e4542bad349337af
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-07-22 16:08:19 -0500

    Update verify-code.ts

--- src/pages/api/verify-code.ts
+++ src/pages/api/verify-code.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const { invite_code } = req.body;
+    const { invite_code, email } = req.body;
 
     return res.status(200).json({
-        result: invite_code === '000-000'
+        result: invite_code === '000-000' && email === 'sam@sam.com'
     });
 };
