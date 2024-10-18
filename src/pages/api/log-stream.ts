294bedc453cf3951e2691e0e74c5a4d59d5a6343
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-10-18 13:06:53 -0500

    Update log-stream.ts

--- src/pages/api/log-stream.ts
+++ src/pages/api/log-stream.ts
@@ -1,10 +1,4 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-//import { headers } from 'next/headers';
-
-export const config = {
-    runtime: 'experimental-edge',
-    location: 'iad1'
-};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 
