b808a0e66b4e5de37b2ce73b384f4ad34c12b48d
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-10-18 12:28:39 -0500

    Update log-stream.ts

--- src/pages/api/log-stream.ts
+++ src/pages/api/log-stream.ts
@@ -1,13 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { headers } from 'next/headers';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const headersList = headers();
-    const dd_api_key = headersList.get('DD-API-KEY');
+    let dd_api_key = req.headers['DD-API-KEY'] ?? ''
 
     console.log('DD-API-KEY', dd_api_key);
 
     return res.status(200).json({
-        success: true
+        success: dd_api_key ?? 'false'
     });
 };
