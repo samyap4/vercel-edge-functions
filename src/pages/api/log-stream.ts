610e717ba2db6b2860f915a6074e8e2c138b219f
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-10-18 13:35:01 -0500

    Update log-stream.ts

--- src/pages/api/log-stream.ts
+++ src/pages/api/log-stream.ts
@@ -1,10 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export const config = {
-    runtime: 'experimental-edge',
-    location: 'iad1'
-  };
-
   export default (req: NextRequest) => {
 
     console.log('req: ', req);
